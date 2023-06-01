30bb4dfc0c67dbe603cdf1134cdf2584377b38e0
PuaZhiXian <xianpua2001@gmail.com>
2023-06-02 00:31:40 +0800

    add dashboard page

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -3,10 +3,18 @@ import {NgModule} from '@angular/core';
 import {DashboardRoutingModule} from './dashboard-routing.module';
 
 import {DashboardComponent} from './page/dashboard/dashboard.component';
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzCardModule} from "ng-zorro-antd/card";
+import {NzCarouselModule} from "ng-zorro-antd/carousel";
+import {CommonModule} from "@angular/common";
+import {NzRateModule} from "ng-zorro-antd/rate";
+import {FormsModule} from "@angular/forms";
+import {NzCollapseModule} from "ng-zorro-antd/collapse";
+import {NzSkeletonModule} from "ng-zorro-antd/skeleton";
 
 
 @NgModule({
-  imports: [DashboardRoutingModule],
+  imports: [DashboardRoutingModule, NzButtonModule, NzCardModule, NzCarouselModule, CommonModule, NzRateModule, FormsModule, NzCollapseModule, NzSkeletonModule],
   declarations: [DashboardComponent],
   exports: [DashboardComponent]
 })
--- src/app/pages/dashboard/page/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/page/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NzCarouselComponent} from "ng-zorro-antd/carousel";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,8 +7,41 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  array = [1, 2, 3, 4];
+  effect = 'scrollx';
+  @ViewChild('recommendCarousel') recommendCarousel!: NzCarouselComponent;
+
+  panels = [
+    {
+      id: '01',
+      active: true,
+      name: 'This is panel header 1',
+      answer: 'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.'
+    },
+    {
+      id: '02',
+      active: false,
+      name: 'This is panel header 2',
+      answer: 'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.'
+    },
+    {
+      id: '03',
+      active: false,
+      name: 'This is panel header 3',
+      answer: 'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.'
+    }
+  ];
+
+  loadingRecommend: boolean = true;
+  loadingFQA: boolean = true;
 
   ngOnInit(): void {
+    this.loadingRecommend = false;
+    this.loadingFQA = false;
+  }
+
+  onCarousel(direction: string) {
+    direction === 'right' ? this.recommendCarousel.next() : this.recommendCarousel.pre();
   }
 
 }

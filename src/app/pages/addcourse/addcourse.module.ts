c653e6f082c46091fe79c2d3e3aa0ae234e15e3e
ChunHan519 <tiowtiow0519@gmail.com>
2023-06-17 23:11:22 +0800

    done addcourse page

--- src/app/pages/addcourse/addcourse.module.ts
+++ src/app/pages/addcourse/addcourse.module.ts
@@ -3,13 +3,18 @@ import {AddcourseRoutingModule} from "./addcourse-routing.module";
 import {NzButtonModule} from "ng-zorro-antd/button";
 import {NzDropDownModule} from "ng-zorro-antd/dropdown";
 import {AddcourseComponent} from "./page/addcourse/addcourse/addcourse.component";
-import {NgForOf, NgIf} from "@angular/common";
+import {JsonPipe, NgForOf, NgIf} from "@angular/common";
 import {NzImageModule} from "ng-zorro-antd/experimental/image";
 import {NzUploadModule} from "ng-zorro-antd/upload";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzGridModule} from "ng-zorro-antd/grid";
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzSelectModule} from "ng-zorro-antd/select";
 
 
 @NgModule({
-  imports: [AddcourseRoutingModule, NzButtonModule, NzDropDownModule, NgForOf, NgIf, NzImageModule, NzUploadModule],
+  imports: [AddcourseRoutingModule, NzButtonModule, NzDropDownModule, NgForOf, NgIf, NzImageModule, NzUploadModule, FormsModule, NzFormModule, NzGridModule, NzInputModule, ReactiveFormsModule, NzSelectModule],
   declarations: [
     AddcourseComponent
   ],

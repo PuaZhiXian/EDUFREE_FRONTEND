1de228f51ea8523deabbbb55bbfb52b758f4f820
oscarhew <xuan.hdx@gmail.com>
2023-06-18 11:19:02 +0800

    add chart api for day and month

--- src/app/pages/profile/page/profile/profile.component.ts
+++ src/app/pages/profile/page/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
-import {IColumnDataPoints} from "../../../../interface/chart/i-column-data-points";
 import {IMyLearning, IMyLearningCategory} from "../../../../interface/learning/i-my-learning";
+import { IColumnDataPoints } from "../../../../interface/chart/i-column-data-points";
 import {ILogout} from "../../../../interface/login/i-logout";
 import {GetAPIService} from "../../../../get-api.service";
 import {finalize} from 'rxjs';
@@ -102,53 +102,43 @@ export class ProfileComponent implements OnInit {
   }
 
   initColumnData() {
-    this.dayColumnDataPoints = [
-      {label: "1 Jan", y: 10},
-      {label: "2 Jan", y: 15},
-      {label: "3 Jan", y: 25},
-      {label: "4 Jan", y: 30},
-      {label: "5 Jan", y: 28},
-      {label: "6 Jan", y: 10},
-      {label: "7 Jan", y: 15},
-      {label: "8 Jan", y: 25},
-      {label: "9 Jan", y: 30},
-      {label: "10 Jan", y: 28},
-      {label: "11 Jan", y: 10},
-      {label: "12 Jan", y: 15},
-      {label: "13 Jan", y: 25},
-      {label: "14 Jan", y: 30},
-      {label: "15 Jan", y: 28},
-      {label: "16 Jan", y: 10},
-      {label: "17 Jan", y: 15},
-      {label: "18 Jan", y: 25},
-      {label: "19 Jan", y: 30},
-      {label: "20 Jan", y: 28},
-      {label: "21 Jan", y: 10},
-      {label: "22 Jan", y: 15},
-      {label: "23 Jan", y: 25},
-      {label: "24 Jan", y: 30},
-      {label: "25 Jan", y: 28},
-      {label: "26 Jan", y: 10},
-      {label: "27 Jan", y: 15},
-      {label: "28 Jan", y: 25},
-      {label: "29 Jan", y: 30},
-      {label: "30 Jan", y: 28},
-      {label: "31 Jan", y: 28}
-    ];
-    this.monthColumnDataPoints = [
-      {label: "Jan", y: 10},
-      {label: "Feb", y: 15},
-      {label: "Mar", y: 25},
-      {label: "Apr", y: 30},
-      {label: "May", y: 28},
-      {label: "Jun", y: 10},
-      {label: "Jul", y: 15},
-      {label: "Aug", y: 25},
-      {label: "Sep", y: 30},
-      {label: "Oct", y: 28},
-      {label: "Nov", y: 10},
-      {label: "Sep", y: 15}
-    ];
+    var newArr: IColumnDataPoints[] = [];
+    this.api.getChartDay().pipe(
+      finalize(() => {
+        this.ref.detectChanges();
+        this.ref.markForCheck();
+      })
+    ).subscribe((resp) => {
+      for (let i = 0; i < resp.length; i++) {
+        var data ={
+          label: resp[i].label,
+          y: Number(resp[i].y)
+        }
+        newArr.push(data);
+      }
+      this.dayColumnDataPoints = newArr;
+
+
+      var newArr2: IColumnDataPoints[] = [];
+      this.api.getChartMonth().pipe(
+        finalize(() => {
+          this.ref.detectChanges();
+          this.ref.markForCheck();
+        })
+      ).subscribe((respond) => {
+        for (let i = 0; i < respond.length; i++) {
+          var data ={
+            label: respond[i].label,
+            y: Number(respond[i].y)
+          }
+          newArr2.push(data);
+        }
+        this.monthColumnDataPoints = newArr2;
+        
+      })
+      
+    })
+    
   }
 
   initMyLearningData() {
@@ -170,34 +160,34 @@ export class ProfileComponent implements OnInit {
 
 
     this.monthMyLearningData = [
-      {
-        id: "1",
-        courseName: 'Introduction to Data Science',
-        description: 'N/A',
-        progress: 50,
-        img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRxNhdWds9jljKiw7UEWef9tCslalAucMkUu9z4GkC8-Bfucvpr'
-      },
-      {
-        id: "1",
-        courseName: 'UI/UX Design',
-        description: 'N/A',
-        progress: 90,
-        img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSyZEbbB9vP9PtVmYKk3N2d_YZzk3nBjS0hrMZrlRSLEtyKCPZ_'
-      },
-      {
-        id: "1",
-        courseName: 'Fundamental of Web Programming',
-        description: 'Creating a website...',
-        progress: 20,
-        img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTg5j5w2xybUndnmFDBn19mJCk0Vd6BT3wmSuxGnXXwG0exeP9G'
-      },
-      {
-        id: "1",
-        courseName: 'Digital Marketing 101',
-        description: 'Marketing strategies and concepts for beginners',
-        progress: 100,
-        img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT2jlelmdJPCLa6V9cCTnUk81iEfS8N6uVAmLtT8FYdrHqK4mf2'
-      }
+      // {
+      //   id: "1",
+      //   courseName: 'Introduction to Data Science',
+      //   description: 'N/A',
+      //   progress: 50,
+      //   img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRxNhdWds9jljKiw7UEWef9tCslalAucMkUu9z4GkC8-Bfucvpr'
+      // },
+      // {
+      //   id: "1",
+      //   courseName: 'UI/UX Design',
+      //   description: 'N/A',
+      //   progress: 90,
+      //   img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSyZEbbB9vP9PtVmYKk3N2d_YZzk3nBjS0hrMZrlRSLEtyKCPZ_'
+      // },
+      // {
+      //   id: "1",
+      //   courseName: 'Fundamental of Web Programming',
+      //   description: 'Creating a website...',
+      //   progress: 20,
+      //   img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTg5j5w2xybUndnmFDBn19mJCk0Vd6BT3wmSuxGnXXwG0exeP9G'
+      // },
+      // {
+      //   id: "1",
+      //   courseName: 'Digital Marketing 101',
+      //   description: 'Marketing strategies and concepts for beginners',
+      //   progress: 100,
+      //   img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT2jlelmdJPCLa6V9cCTnUk81iEfS8N6uVAmLtT8FYdrHqK4mf2'
+      // }
     ]
 
   }
@@ -234,36 +224,36 @@ export class ProfileComponent implements OnInit {
       this.selectingMyLearningData = resp;
     })
 
-    this.selectingMyLearningData = [
-      {
-        id: "1",
-        courseName: 'Introduction to Data Science',
-        description: 'N/A',
-        progress: 50,
-        img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRxNhdWds9jljKiw7UEWef9tCslalAucMkUu9z4GkC8-Bfucvpr'
-      },
-      {
-        id: "1",
-        courseName: 'UI/UX Design',
-        description: 'N/A',
-        progress: 90,
-        img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSyZEbbB9vP9PtVmYKk3N2d_YZzk3nBjS0hrMZrlRSLEtyKCPZ_'
-      },
-      {
-        id: "1",
-        courseName: 'Fundamental of Web Programming',
-        description: 'Creating a website...',
-        progress: 20,
-        img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTg5j5w2xybUndnmFDBn19mJCk0Vd6BT3wmSuxGnXXwG0exeP9G'
-      },
-      {
-        id: "1",
-        courseName: 'Digital Marketing 101',
-        description: 'Marketing strategies and concepts for beginners',
-        progress: 100,
-        img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT2jlelmdJPCLa6V9cCTnUk81iEfS8N6uVAmLtT8FYdrHqK4mf2'
-      }
-    ]
+    // this.selectingMyLearningData = [
+    //   {
+    //     id: "1",
+    //     courseName: 'Introduction to Data Science',
+    //     description: 'N/A',
+    //     progress: 50,
+    //     img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRxNhdWds9jljKiw7UEWef9tCslalAucMkUu9z4GkC8-Bfucvpr'
+    //   },
+    //   {
+    //     id: "1",
+    //     courseName: 'UI/UX Design',
+    //     description: 'N/A',
+    //     progress: 90,
+    //     img: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSyZEbbB9vP9PtVmYKk3N2d_YZzk3nBjS0hrMZrlRSLEtyKCPZ_'
+    //   },
+    //   {
+    //     id: "1",
+    //     courseName: 'Fundamental of Web Programming',
+    //     description: 'Creating a website...',
+    //     progress: 20,
+    //     img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTg5j5w2xybUndnmFDBn19mJCk0Vd6BT3wmSuxGnXXwG0exeP9G'
+    //   },
+    //   {
+    //     id: "1",
+    //     courseName: 'Digital Marketing 101',
+    //     description: 'Marketing strategies and concepts for beginners',
+    //     progress: 100,
+    //     img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT2jlelmdJPCLa6V9cCTnUk81iEfS8N6uVAmLtT8FYdrHqK4mf2'
+    //   }
+    // ]
   }
 
 

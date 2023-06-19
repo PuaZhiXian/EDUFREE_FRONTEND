fcd8e6e0a263246ee10773a8a1d49d10556b738e
PuaZhiXian <xianpua2001@gmail.com>
2023-06-19 22:56:43 +0800

    remove duplicate displayName

--- src/app/interface/courses/i-course-category.ts
+++ src/app/interface/courses/i-course-category.ts
@@ -1,7 +1,6 @@
 import {ICourseDetail} from "./i-course-detail";
 
 export interface ICourseCategory {
-  displayName: string,
   category: string,
   courses: ICourseDetail[]
   displayName:string;
